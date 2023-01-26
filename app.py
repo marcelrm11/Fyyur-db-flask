5795f311f7d1c67e18344444c2bd00e6728f929f
marcelrm11 <marcelrm11@gmail.com>
2023-01-26 14:39:39 +0100

    show create

--- app.py
+++ app.py
@@ -600,12 +600,41 @@ def create_shows():
 def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
+    form = ShowForm(request.form, meta={'csrf': False})
+        # validate all fields
+    if form.validate():
+        # prepare for transaction
+        try:
+            artist = Artist.query.get(form.artist_id.data)
+            venue = Venue.query.get(form.venue_id.data)
+            show = Show(
+                artist_id=artist.id,
+                artist_name = artist.name,
+                artist_image_link = artist.image_link,
+                venue_id=venue.id,
+                venue_name = venue.name,
+                start_time=form.start_time.data
+            )
+            db.session.add(show)
+            db.session.commit()
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
+            # on successful db insert, flash success
+            flash('Show was successfully listed!')
+        except ValueError as e:
+            print(e, sys.exc_info())
+            db.session.rollback()
     # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+            flash('An error occurred. Show could not be listed.')
+        finally:
+            db.session.close()
+    # If there is an invalid field
+    else:
+        message = []
+        for field, err in form.errors.items():
+            message.append(field + ' ' + '|'.join(err))
+        flash('Errors ' + str(message))
+        form = ArtistForm()
+        return render_template('forms/new_show.html', form=form)
     return render_template('pages/home.html')
 
 
